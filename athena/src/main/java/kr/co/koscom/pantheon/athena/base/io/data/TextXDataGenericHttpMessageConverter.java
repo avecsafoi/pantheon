b2733a0e9ddb53bb6119c5fecdeb9a354235caeb
avecsafoi <avecsafoi@gmail.com>
2025-08-01 12:16:34 +0900

    .

--- athena/src/main/java/kr/co/koscom/pantheon/athena/base/io/data/TextXDataGenericHttpMessageConverter.java
+++ athena/src/main/java/kr/co/koscom/pantheon/athena/base/io/data/TextXDataGenericHttpMessageConverter.java
@@ -14,102 +14,129 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TextXDataGenericHttpMessageConverter extends TextXDataHttpMessageConverter implements GenericHttpMessageConverter<Object> {
 
     @Override
-    public boolean canRead(@Nonnull Type type, @Nullable Class<?> contextClass, @Nullable MediaType mediaType) {
-        if (type instanceof ParameterizedType p)
-            for (Type t : p.getActualTypeArguments())
-                if (super.canRead((Class<?>) t, mediaType)) return true;
-        return (type instanceof Class<?> c) && super.canRead(c, mediaType);
-    }
-
-    @Override
-    public @Nonnull Object read(@Nonnull Type type, @Nullable Class<?> contextClass, @Nonnull HttpInputMessage m) throws IOException, HttpMessageNotReadableException {
-        try {
-            MediaType t = m.getHeaders().getContentType();
-            Charset charset = t != null && t.getCharset() != null ? t.getCharset() : defaultCharset;
-            TextXDataInputStream tdis = new TextXDataInputStream(m.getBody(), charset);
-            if (type instanceof ParameterizedType p) {
-                Class<?> c = (Class<?>) p.getRawType();
-                if (List.class.isAssignableFrom(c)) {
-                    Class<?> s = (Class<?>) p.getActualTypeArguments()[0];
-                    List<Object> l = new ArrayList<>();
-                    while (tdis.available() >= 0) l.add(tdis.readObject(s));
-                    return l;
-                }
-                if (Map.class.isAssignableFrom(c)) {
-                    Class<?> a = (Class<?>) p.getActualTypeArguments()[0];
-                    Class<?> b = (Class<?>) p.getActualTypeArguments()[1];
-                    HashMap<Object, Object> h = new HashMap<>();
-                    while (tdis.available() >= 0) h.put(tdis.readObject(a), tdis.readObject(b));
-                    return h;
-                }
-                if (c.isArray()) {
-                    Class<?> s = (Class<?>) p.getActualTypeArguments()[0];
-                    List<Object> l = new ArrayList<>();
-                    while (tdis.available() >= 0) l.add(tdis.readObject(s));
-                    Object a = Array.newInstance(s, l.size());
-                    for (int i = 0; i < l.size(); i++) Array.set(a, i, l.get(i));
-                    return a;
-                }
-                throw new IOException("Unsupported Generic type: %s" + c.getName());
-            }
-            return tdis.readObject((Class<?>) type);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public boolean canWrite(@Nullable Type type, @Nonnull Class<?> clazz, @Nullable MediaType mediaType) {
-        return type != null && canRead(type, clazz, mediaType);
-    }
-
-
-    @Override
-    public void write(@Nonnull Object o, @Nullable Type type, @Nullable MediaType t, @Nonnull HttpOutputMessage m) throws IOException, HttpMessageNotWritableException {
-        try {
-            Charset charset = t != null && t.getCharset() != null ? t.getCharset() : defaultCharset;
-            HttpHeaders h = m.getHeaders();
-            if (h.getContentType() == null)
-                h.setContentType(new MediaType("text", "plain", charset));
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            TextXDataOutputStream tdos = new TextXDataOutputStream(baos, charset);
-            if (type instanceof ParameterizedType p) {
-                Class<?> c = (Class<?>) p.getRawType();
-                if (o instanceof List<?> l) {
-                    for (Object a : l) tdos.writeObject(a);
-                } else if (o instanceof Map<?, ?> x) {
-                    for (Map.Entry<?, ?> e : x.entrySet()) {
-                        tdos.writeObject(e.getKey());
-                        tdos.writeObject(e.getValue());
+    public boolean canRead(@Nonnull Type type, @Nullable Class<?> contextClass, @Nullable MediaType mt) {
+        switch (type) {
+            case ParameterizedType pt -> {
+                Type t = pt.getRawType();
+                if (t instanceof Class<?> c) {
+                    if (List.class.isAssignableFrom(c)) {
+                        Type[] a = pt.getActualTypeArguments();
+                        if (a.length == 1 && a[0] instanceof Class<?> x) {
+                            return super.canRead(x, mt);
+                        }
                     }
-                } else if (c.isArray()) {
-                    int z = Array.getLength(o);
-                    for (int i = 0; i < z; i++) tdos.writeObject(Array.get(o, i));
-                } else {
-                    throw new IOException("Unsupported Generic type: %s" + c.getName());
                 }
-            } else {
-                tdos.writeObject(o);
             }
-            tdos.close();
-            byte[] b = baos.toByteArray();
-            h.setContentLength(b.length);
-            OutputStream os = m.getBody();
-            os.write(b);
-            os.flush();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            case GenericArrayType at -> {
+                if (at.getGenericComponentType() instanceof Class<?> c) {
+                    return super.canRead(c, mt);
+                }
+            }
+            case Class<?> c -> {
+                if (c.isArray()) {
+                    return super.canRead(c.getComponentType(), mt);
+                } else {
+                    return super.canRead(c, mt);
+                }
+            }
+            default -> {
+
+            }
         }
+        return false;
+    }
+
+    @Override
+    public @Nonnull Object read(@Nonnull Type type, @Nullable Class<?> contextClass, @Nonnull HttpInputMessage im) throws IOException, HttpMessageNotReadableException {
+        MediaType mt = im.getHeaders().getContentType();
+        Charset charset = mt != null && mt.getCharset() != null ? mt.getCharset() : defaultCharset;
+        TextXDataInputStream tdis = new TextXDataInputStream(im.getBody(), charset);
+        switch (type) {
+            case ParameterizedType pt -> {
+                Type t = pt.getRawType();
+                if (t instanceof Class<?> c) {
+                    if (List.class.isAssignableFrom(c)) {
+                        Type[] a = pt.getActualTypeArguments();
+                        if (a.length == 1 && a[0] instanceof Class<?> x) {
+                            List<Object> list = new ArrayList<>();
+                            while (tdis.available() > 0) {
+                                Object o = tdis.readObject(x);
+                                list.add(o);
+                            }
+                            return list;
+                        }
+                    }
+                }
+            }
+            case GenericArrayType at -> {
+                if (at.getGenericComponentType() instanceof Class<?> x) {
+                    List<Object> list = new ArrayList<>();
+                    while (tdis.available() > 0) {
+                        Object o = tdis.readObject(x);
+                        list.add(o);
+                    }
+                    Object ro = Array.newInstance(x, list.size());
+                    for (int i = 0; i < list.size(); i++) Array.set(ro, i, list.get(i));
+                    return ro;
+                }
+            }
+            case Class<?> c -> {
+                if (c.isArray()) {
+                    List<Object> list = new ArrayList<>();
+                    while (tdis.available() > 0) {
+                        Object o = tdis.readObject(c.getComponentType());
+                        list.add(o);
+                    }
+                    Object ro = Array.newInstance(c.getComponentType(), list.size());
+                    for (int i = 0; i < list.size(); i++) Array.set(ro, i, list.get(i));
+                    return ro;
+                } else {
+                    return tdis.readObject(c);
+                }
+            }
+            default -> {
+
+            }
+        }
+        throw new IOException("Unexpected Type " + type.getTypeName());
+    }
+
+    @Override
+    public boolean canWrite(@Nullable Type type, @Nonnull Class<?> clazz, @Nullable MediaType mt) {
+        return type != null && canRead(type, clazz, mt);
+    }
+
+    @Override
+    public void write(@Nonnull Object o, @Nullable Type type, @Nullable MediaType mt, @Nonnull HttpOutputMessage om) throws IOException, HttpMessageNotWritableException {
+        HttpHeaders h = om.getHeaders();
+        if (mt == null) mt = h.getContentType();
+        Charset charset = mt != null && mt.getCharset() != null ? mt.getCharset() : defaultCharset;
+        if (h.getContentType() == null) h.setContentType(new MediaType("text", "plain", charset));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        TextXDataOutputStream tdos = new TextXDataOutputStream(baos, charset);
+        if (o instanceof List<?> l) {
+            for (Object a : l) tdos.writeObject(a);
+        } else if (o.getClass().isArray()) {
+            int z = Array.getLength(o);
+            for (int i = 0; i < z; i++) tdos.writeObject(Array.get(o, i));
+        } else {
+            tdos.writeObject(o);
+        }
+        tdos.close();
+        byte[] b = baos.toByteArray();
+        h.setContentLength(b.length);
+        OutputStream os = om.getBody();
+        os.write(b);
+        os.flush();
     }
 }
--- athena/src/test/java/테스트.java
+++ athena/src/test/java/테스트.java
@@ -1,10 +1,12 @@
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.commons.lang3.reflect.MethodUtils;
+import kr.co.koscom.pantheon.athena.base.io.data.TextXData;
+import org.apache.commons.lang3.StringUtils;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class 테스트 {
@@ -12,23 +14,44 @@ public class 테스트 {
     public List<String> fs;
 
     public static void main(String[] args) {
-        Method m = MethodUtils.getMethodObject(테스트.class, "test", List.class);
-        ParameterizedType mt = (ParameterizedType) m.getGenericReturnType();
-        Class<?> returnType = m.getReturnType();
-        System.out.printf("returnType = %s", returnType.getSimpleName());
-        TypeVariable<? extends Class<?>>[] tva = returnType.getTypeParameters();
-        int i = 0;
-        for (TypeVariable<? extends Class<?>> a : tva) {
-            System.out.printf("%3d %s (%s) %s", i++, a.getName(), a.getClass().getSimpleName(), a.getGenericDeclaration());
-        }
-        Field f = FieldUtils.getField(테스트.class, "fs", true);
-        ParameterizedType ft = (ParameterizedType) f.getGenericType();
-
-
-        System.out.println("");
+        ArrayList<TextXData> o = new ArrayList<TextXData>();
+        test(o.getClass(), 0);
+        TextXData[] tx = {};
+        test(tx.getClass(), 0);
+        Integer x = 0;
+        test(x.getClass(), 0);
     }
 
-    public List<String> test(List<String> in) {
-        return in;
+    public static void test(Type type, int i) {
+
+        System.out.println();
+
+        String ds = StringUtils.repeat(' ', i * 4);
+
+        System.out.println(ds + "TEST: " + type.getTypeName());
+
+        if (type instanceof ParameterizedType pt) {
+            System.out.println(ds + "ParameterizedTypetype: " + pt.getTypeName());
+            Type rt = pt.getRawType();
+            Class<?> rtc = rt.getClass();
+            for (Type t : pt.getActualTypeArguments())
+                test(t, i + 1);
+        }
+
+        if (type instanceof GenericArrayType gat) {
+            System.out.println(ds + "GenericArrayType: " + gat.getTypeName());
+        }
+
+        if (type instanceof Class<?> c) {
+            TypeVariable<? extends Class<?>>[] tva = c.getTypeParameters();
+            System.out.println(ds + "getTypeParameters: " + Arrays.toString(tva));
+
+            Class<?> s = c.getComponentType();
+            if (s != null) System.out.println(ds + "getComponentType: " + s.getCanonicalName());
+
+            System.out.println("Class: " + c.getCanonicalName());
+        } else {
+            throw new RuntimeException("NOT CLASS: " + type.getTypeName());
+        }
     }
 }
