03547bc3d538c79d7611d940cde47a3c41e6631a
avecsafoi <avecsafoi@gmail.com>
2025-08-12 14:21:31 +0900

    vue js added

--- pb/src/main/java/kr/co/koscom/olympus/pb/PbApplication.java
+++ pb/src/main/java/kr/co/koscom/olympus/pb/PbApplication.java
@@ -1,6 +1,7 @@
 package kr.co.koscom.olympus.pb;
 
 import kr.co.koscom.olympus.pb.ab.data.converter.PBTextDataHttpMessageConverter;
+import kr.co.koscom.olympus.pb.ab.db.page.PBMyBatisInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +18,11 @@ public class PbApplication {
     @Description("송수신 전문을 파싱하는 기능")
     public PBTextDataHttpMessageConverter pbTextDataHttpMessageConverter() {
         return new PBTextDataHttpMessageConverter();
+    }
+
+    @Bean
+    @Description("쿼리구문에 연속조회 구문을 추가하는 기능")
+    public PBMyBatisInterceptor pbMyBatisInterceptor() {
+        return new PBMyBatisInterceptor();
     }
 }
