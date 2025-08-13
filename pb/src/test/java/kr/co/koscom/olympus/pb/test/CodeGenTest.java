7f976b9461d152bf2186f80954e35acf0f9d9693
avecsafoi <avecsafoi@gmail.com>
2025-08-13 11:18:03 +0900

    vue js added

--- pb/src/test/java/kr/co/koscom/olympus/pb/test/CodeGenTest.java
+++ pb/src/test/java/kr/co/koscom/olympus/pb/test/CodeGenTest.java
@@ -1,7 +1,6 @@
 package kr.co.koscom.olympus.pb.test;
 
 import com.mybatisflex.codegen.Generator;
-import com.mybatisflex.codegen.config.ColumnConfig;
 import com.mybatisflex.codegen.config.GlobalConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.Resource;
@@ -22,6 +21,7 @@ public class CodeGenTest {
         GlobalConfig globalConfig = new GlobalConfig();
 
         // 루트 패키지 설정
+        globalConfig.setSourceDir("../auto");
         globalConfig.setBasePackage("kr.co.koscom.olympus.pb.db");
 
         // 테이블 접두사 및 생성대상 테이블 (대상 테이블을 설정하지 않으면 모든 테이블)
@@ -52,38 +52,6 @@ public class CodeGenTest {
 //        columnConfig.setLarge(true);
 //        columnConfig.setVersion(true);
 //        globalConfig.setColumnConfig("tb_account", columnConfig);
-
-        return globalConfig;
-    }
-
-    public static GlobalConfig createGlobalConfigUseStyle2() {
-        // 구성 콘텐츠를 생성합니다.
-        GlobalConfig globalConfig = new GlobalConfig();
-
-        // 루트 패키지를 설정합니다.
-        globalConfig.getPackageConfig().setBasePackage("com.test");
-
-        // 테이블 접두사와 생성할 테이블을 설정합니다.
-        // setGenerateTable을 구성하지 않으면 모든 테이블이 생성됩니다.
-        globalConfig.getStrategyConfig()
-                .setTablePrefix("tb_")
-                .setGenerateTable("tb_account", "tb_account_session");
-
-        // 엔티티 생성을 설정하고 Lombok을 활성화하세요
-        globalConfig.enableEntity()
-                .setWithLombok(true)
-                .setJdkVersion(24);
-
-        // 생성된 매퍼 설정
-        globalConfig.enableMapper();
-
-        // 열을 개별적으로 구성할 수 있습니다
-        ColumnConfig columnConfig = new ColumnConfig();
-        columnConfig.setColumnName("tenant_id");
-        columnConfig.setLarge(true);
-        columnConfig.setVersion(true);
-        globalConfig.getStrategyConfig()
-                .setColumnConfig("tb_account", columnConfig);
 
         return globalConfig;
     }
