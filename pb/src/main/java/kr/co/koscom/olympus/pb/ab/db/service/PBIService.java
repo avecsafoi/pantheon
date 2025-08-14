3e61ed10b957b400f6eb23e290d91472ace775c7
avecsafoi <avecsafoi@gmail.com>
2025-08-14 09:37:13 +0900

    .

---
+++ pb/src/main/java/kr/co/koscom/olympus/pb/ab/db/service/PBIService.java
@@ -0,0 +1,14 @@
+package kr.co.koscom.olympus.pb.ab.db.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import kr.co.koscom.olympus.pb.ab.db.page.PBCPage;
+
+import java.util.List;
+
+public interface PBIService<T> extends IService<T> {
+
+    public List<T> cpage(PBCPage pg);
+
+    public List<T> cpage(PBCPage pg, QueryWrapper qw);
+}
