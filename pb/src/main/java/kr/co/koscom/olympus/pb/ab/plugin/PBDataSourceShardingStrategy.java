3f791956ee263fd3ff0c0d3092af8732cf04fe13
avecsafoi <avecsafoi@gmail.com>
2025-08-16 17:19:43 +0900

    .

--- pb/src/main/java/kr/co/koscom/olympus/pb/ab/plugin/PBDataSourceShardingStrategy.java
+++ pb/src/main/java/kr/co/koscom/olympus/pb/ab/plugin/PBDataSourceShardingStrategy.java
@@ -6,9 +6,11 @@ import java.lang.reflect.Method;
 
 public class PBDataSourceShardingStrategy implements DataSourceShardingStrategy {
 
-    public String doSharding(String currentDataSourceKey, Object mapper, Method mapperMethod, Object[] methodArgs) {
+    public String doSharding(String k, Object mapper, Method mapperMethod, Object[] methodArgs) {
         String n = mapperMethod.getName();
-        if (n.matches("(insert|update|delete)")) return "ds";
-        return currentDataSourceKey;
+        if (k != null && n.matches("(insert|update|delete)")) {
+            return k.replaceAll("[03456789]\\d*$", "");
+        }
+        return k;
     }
 }
