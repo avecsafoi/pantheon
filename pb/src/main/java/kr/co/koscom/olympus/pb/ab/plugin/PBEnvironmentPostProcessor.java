7cb0c774050ae30a055c0715cbc50df6e536a037
KOSCOM <KOSCOM@host.docker.internal>
2025-08-28 08:41:56 +0900

    .

--- pb/src/main/java/kr/co/koscom/olympus/pb/ab/plugin/PBEnvironmentPostProcessor.java
+++ pb/src/main/java/kr/co/koscom/olympus/pb/ab/plugin/PBEnvironmentPostProcessor.java
@@ -1,4 +1,10 @@
+
 package kr.co.koscom.olympus.pb.ab.plugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.salt.RandomSaltGenerator;
@@ -9,49 +15,42 @@ import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertySource;
 import org.springframework.lang.NonNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class PBEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
 
-        StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
-        enc.setSaltGenerator(new RandomSaltGenerator());
-        enc.setAlgorithm("PBEWithMD5AndDES");
-        enc.setStringOutputType("BASE64");
-        enc.setPassword("KOSCOM");
+		StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
+		enc.setSaltGenerator(new RandomSaltGenerator());
+		enc.setAlgorithm("PBEWithMD5AndDES");
+		enc.setStringOutputType("BASE64");
+		enc.setPassword("KOSCOM");
 
-        Pattern p1 = Pattern.compile("ENC\\(([^()]*)\\)");
-        MutablePropertySources ms = env.getPropertySources();
-        for (PropertySource p : ms) {
-            if (p.getSource() instanceof Map m) {
-                int[] i = {0};
-                HashMap<Object, Object> map = new HashMap<>(m);
-                for (Map.Entry<Object, Object> e : map.entrySet()) {
-                    Matcher m1 = p1.matcher(e.getValue().toString());
-                    if (m1.find()) {
-                        String rv = m1.group(1);
-                        String xv = enc.decrypt(rv);
-                        e.setValue(xv);
-                        i[0]++;
-                    }
-                }
-                if (i[0] > 0) {
-                    String n = p.getName();
-                    PropertySource x = new PropertySource<Map<Object, Object>>(n, map) {
-                        @Override
-                        public Object getProperty(@NonNull String name) {
-                            return source.get(name);
-                        }
-                    };
-                    ms.replace(n, x);
-                }
-            }
-        }
+		Map map = new HashMap<>();
+
+		Pattern p1 = Pattern.compile("ENC\\(([^()]*)\\)");
+		MutablePropertySources ms = env.getPropertySources();
+
+		for (PropertySource p : ms)
+			if (p.getSource() instanceof Map m)
+				m.forEach((k, v) -> {
+					Matcher t = p1.matcher(v.toString());
+					if (t.find()) {
+						String rv = t.group(1);
+						String xv = enc.decrypt(rv);
+						map.put(k, xv);
+					}
+				});
+
+		if (!map.isEmpty()) {
+			PropertySource p = new PropertySource<Map<Object, Object>>("PB_ENC", map) {
+				@Override
+				public Object getProperty(@NonNull String name) {
+					return source.get(name);
+				}
+			};
+			ms.addFirst(p);
+		}
     }
 }
